109ce2834c18eb7a43e3e29149c7395a4cfdc122
syjoe <syee2joe@gmail.com>
2024-08-08 21:07:19 +0900

    init

---
+++ src/main/java/com/baeda/baeda/BaeDaApplication.java
@@ -0,0 +1,13 @@
+package com.baeda.baeda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BaeDaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BaeDaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/baeda/baeda/BaeDaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.baeda.baeda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BaeDaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
