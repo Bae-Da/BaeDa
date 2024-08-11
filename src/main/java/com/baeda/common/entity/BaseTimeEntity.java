38a8408e6f76fa6e4303cfa840e75b2256a16379
hyezuu <duudu0704@gmail.com>
2024-08-11 15:44:50 +0900

    feat(common): base 엔티티 구현

---
+++ src/main/java/com/baeda/common/entity/BaseTimeEntity.java
@@ -0,0 +1,17 @@
+package com.baeda.common.entity;
+
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import java.time.LocalDateTime;
+import lombok.Getter;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+@Getter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public abstract class BaseTimeEntity extends BaseCreatedTimeEntity{
+
+    @LastModifiedDate
+    private LocalDateTime updatedAt;
+}
