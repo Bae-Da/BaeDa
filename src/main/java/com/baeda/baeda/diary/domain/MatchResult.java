5f2986e7b9b2d1e2a7eb45e2f85d14c0db7ab9f2
syjoe <syee2joe@gmail.com>
2024-08-14 00:57:04 +0900

    yml trouble shooting

---
+++ src/main/java/com/baeda/baeda/diary/domain/MatchResult.java
@@ -0,0 +1,5 @@
+package com.baeda.baeda.diary.domain;
+
+public enum MatchResult {
+    WIN, DRAW, LOSE
+}
---
+++ src/main/java/com/baeda/baeda/diary/domain/ViewingMethod.java
@@ -0,0 +1,5 @@
+package com.baeda.baeda.diary.domain;
+
+public enum ViewingMethod {
+    HOME_VIEWING, LIVE_ATTENDANCE
+}
