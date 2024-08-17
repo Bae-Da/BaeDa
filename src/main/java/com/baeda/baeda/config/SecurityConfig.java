ec710ff8780a70aefc8677598317be66af720f37
hyezuu <duudu0704@gmail.com>
2024-08-17 18:53:32 +0900

    feat(user): security 기본 틀 구현

--- src/main/java/com/baeda/baeda/config/SecurityConfig.java
+++ src/main/java/com/baeda/baeda/config/SecurityConfig.java
@@ -1,12 +1,48 @@
 package com.baeda.baeda.config;
 
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer.FrameOptionsConfig;
+import org.springframework.security.config.annotation.web.configurers.LogoutConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
+@EnableWebSecurity
 @Configuration
 public class SecurityConfig {
+
+    @Bean
+    public WebSecurityCustomizer configure() {
+        return web -> web.ignoring()
+            .requestMatchers(PathRequest
+                .toStaticResources()
+                .atCommonLocations()
+            );
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        return http
+            .authorizeHttpRequests(auth ->
+                auth.requestMatchers("/**").permitAll()
+                    .anyRequest().permitAll()
+            )
+            .formLogin(form -> form
+                .loginPage("/login")
+                .defaultSuccessUrl("/home")
+                .permitAll()
+            )
+            .csrf(AbstractHttpConfigurer::disable)
+            .headers(headers -> headers.frameOptions(FrameOptionsConfig::sameOrigin))
+            .logout(LogoutConfigurer::permitAll)
+            .build();
+    }
 
     @Bean
     PasswordEncoder passwordEncoder() {
